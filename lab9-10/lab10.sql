416a92eb300f5a82b12d99114812600f4d5bef5d
NaStacy <nasty0701@gmail.com>
2022-07-29 16:03:30 +0300

    U1M10

---
+++ lab9-10/lab10.sql
@@ -0,0 +1,199 @@
+
+--The creation of calendar
+--select * from calendar;
+drop table calendar;
+CREATE TABLE calendar AS (SELECT * FROM
+(SELECT 
+  TRUNC( sd + rn ) time_id,
+  TO_CHAR( sd + rn, 'fmDay' ) day_name,
+  TO_CHAR( sd + rn, 'D' ) day_number_in_week,
+  TO_CHAR( sd + rn, 'DD' ) day_number_in_month,
+  TO_CHAR( sd + rn, 'DDD' ) day_number_in_year,
+  TO_CHAR( sd + rn, 'W' ) calendar_week_number,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'D' ) IN ( 1, 2, 3, 4, 5, 6 ) THEN
+        NEXT_DAY( sd + rn, 'œŒÕ≈ƒ≈À‹Õ» ' )
+      ELSE
+        ( sd + rn )
+    END ) week_ending_date,
+  TO_CHAR( sd + rn, 'MM' ) calendar_month_number,
+  TO_CHAR( LAST_DAY( sd + rn ), 'DD' ) days_in_cal_month,
+  LAST_DAY( sd + rn ) end_of_cal_month,
+  TO_CHAR( sd + rn, 'FMMonth' ) calendar_month_name,
+  ( ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) - TRUNC( sd + rn, 'Q' ) + 1 ) days_in_cal_quarter,
+  TRUNC( sd + rn, 'Q' ) beg_of_cal_quarter,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) end_of_cal_quarter,
+  TO_CHAR( sd + rn, 'Q' ) calendar_quarter_number,
+  TO_CHAR( sd + rn, 'YYYY' ) calendar_year,
+  ( TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    - TRUNC( sd + rn, 'YEAR' ) ) days_in_cal_year,
+  TRUNC( sd + rn, 'YEAR' ) beg_of_cal_year,
+  TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' ) end_of_cal_year
+FROM
+  ( 
+    SELECT 
+      TO_DATE( '12/31/500', 'MM/DD/YYYY' ) sd,
+      rownum rn
+    FROM dual
+      CONNECT BY level <= 1500000  ))
+);
+
+----------------------task 1------------------------------
+EXPLAIN PLAN FOR
+SELECT * FROM calendar 
+WHERE day_name = '—Û··ÓÚ‡';
+
+select plan_table_output from table(dbms_xplan.display());
+
+EXPLAIN PLAN FOR
+SELECT /*+ parallel(TIMES, 4)*/* FROM calendar 
+WHERE day_name = '—Û··ÓÚ‡';
+
+select plan_table_output from table(dbms_xplan.display());
+
+----------------------task 2------------------------------
+EXPLAIN PLAN FOR
+DELETE FROM calendar
+WHERE calendar_week_number IN(2, 1);
+
+select plan_table_output from table(dbms_xplan.display());
+
+EXPLAIN PLAN FOR
+DELETE /*+ parallel(TIMES, 4)*/ FROM calendar
+WHERE calendar_week_number IN(2, 1);
+
+select plan_table_output from table(dbms_xplan.display());
+
+----------------------task 3------------------------------
+drop table calendar;
+EXPLAIN PLAN FOR 
+CREATE TABLE calendar AS (SELECT * FROM
+(SELECT 
+  TRUNC( sd + rn ) time_id,
+  TO_CHAR( sd + rn, 'fmDay' ) day_name,
+  TO_CHAR( sd + rn, 'D' ) day_number_in_week,
+  TO_CHAR( sd + rn, 'DD' ) day_number_in_month,
+  TO_CHAR( sd + rn, 'DDD' ) day_number_in_year,
+  TO_CHAR( sd + rn, 'W' ) calendar_week_number,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'D' ) IN ( 1, 2, 3, 4, 5, 6 ) THEN
+        NEXT_DAY( sd + rn, 'œŒÕ≈ƒ≈À‹Õ» ' )
+      ELSE
+        ( sd + rn )
+    END ) week_ending_date,
+  TO_CHAR( sd + rn, 'MM' ) calendar_month_number,
+  TO_CHAR( LAST_DAY( sd + rn ), 'DD' ) days_in_cal_month,
+  LAST_DAY( sd + rn ) end_of_cal_month,
+  TO_CHAR( sd + rn, 'FMMonth' ) calendar_month_name,
+  ( ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) - TRUNC( sd + rn, 'Q' ) + 1 ) days_in_cal_quarter,
+  TRUNC( sd + rn, 'Q' ) beg_of_cal_quarter,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) end_of_cal_quarter,
+  TO_CHAR( sd + rn, 'Q' ) calendar_quarter_number,
+  TO_CHAR( sd + rn, 'YYYY' ) calendar_year,
+  ( TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    - TRUNC( sd + rn, 'YEAR' ) ) days_in_cal_year,
+  TRUNC( sd + rn, 'YEAR' ) beg_of_cal_year,
+  TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' ) end_of_cal_year
+FROM
+  ( 
+    SELECT 
+      TO_DATE( '12/31/500', 'MM/DD/YYYY' ) sd,
+      rownum rn
+    FROM dual
+      CONNECT BY level <= 1500000  ))
+);
+select plan_table_output from table(dbms_xplan.display());
+
+
+drop table calendar;
+EXPLAIN PLAN FOR 
+CREATE TABLE calendar parallel 8 AS (SELECT * FROM
+(SELECT 
+  TRUNC( sd + rn ) time_id,
+  TO_CHAR( sd + rn, 'fmDay' ) day_name,
+  TO_CHAR( sd + rn, 'D' ) day_number_in_week,
+  TO_CHAR( sd + rn, 'DD' ) day_number_in_month,
+  TO_CHAR( sd + rn, 'DDD' ) day_number_in_year,
+  TO_CHAR( sd + rn, 'W' ) calendar_week_number,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'D' ) IN ( 1, 2, 3, 4, 5, 6 ) THEN
+        NEXT_DAY( sd + rn, 'œŒÕ≈ƒ≈À‹Õ» ' )
+      ELSE
+        ( sd + rn )
+    END ) week_ending_date,
+  TO_CHAR( sd + rn, 'MM' ) calendar_month_number,
+  TO_CHAR( LAST_DAY( sd + rn ), 'DD' ) days_in_cal_month,
+  LAST_DAY( sd + rn ) end_of_cal_month,
+  TO_CHAR( sd + rn, 'FMMonth' ) calendar_month_name,
+  ( ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) - TRUNC( sd + rn, 'Q' ) + 1 ) days_in_cal_quarter,
+  TRUNC( sd + rn, 'Q' ) beg_of_cal_quarter,
+  ( CASE
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 1 THEN
+        TO_DATE( '03/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 2 THEN
+        TO_DATE( '06/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 3 THEN
+        TO_DATE( '09/30/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+      WHEN TO_CHAR( sd + rn, 'Q' ) = 4 THEN
+        TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    END ) end_of_cal_quarter,
+  TO_CHAR( sd + rn, 'Q' ) calendar_quarter_number,
+  TO_CHAR( sd + rn, 'YYYY' ) calendar_year,
+  ( TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' )
+    - TRUNC( sd + rn, 'YEAR' ) ) days_in_cal_year,
+  TRUNC( sd + rn, 'YEAR' ) beg_of_cal_year,
+  TO_DATE( '12/31/' || TO_CHAR( sd + rn, 'YYYY' ), 'MM/DD/YYYY' ) end_of_cal_year
+FROM
+  ( 
+    SELECT 
+      TO_DATE( '12/31/500', 'MM/DD/YYYY' ) sd,
+      rownum rn
+    FROM dual
+      CONNECT BY level <= 1500000  ))
+);
+select plan_table_output from table(dbms_xplan.display());
